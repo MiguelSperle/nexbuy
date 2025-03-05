adc13b9e2380a3fb7b0ede4557fee20a868c31ac
MiguelSperle <miguelsperledeveloper@gmail.com>
2025-03-05 00:08:29 -0300

    starting configuration

---
+++ src/main/java/com/example/miguelsperle/nexbuy/NexbuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.miguelsperle.nexbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexbuyApplication.class, args);
+	}
+
+}
